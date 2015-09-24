cf06ed9a82a7db4a382c12c6a0083a1d77829df4
Nathnael Atsbeha <Natitad0989@gmaol.com>
2015-09-24 11:44:40 -0700

    add Assignment

---
+++ Assignment.java
@@ -0,0 +1,20 @@
+public class Assignment {     
+	public static void main(String[] args){
+	int n1, n2, n3, n4, n5, n6, n7, n8, n9;
+	Double d1, d2;
+	String s1 = "Beat";
+	String s2 = "Army";
+	String s3 = " + '' + ";
+	d1 = 1.0;d2 = 3.5;
+	n1 = 1;n2 = 2;n3 = 6;n4 = 13;
+	n5 = 35;n6 = 5;n7 = 7;n8 = 3;n9 = 4;
+	System.out.println(n1 + "/" + n2 + "=" + n1/n2);
+	System.out.println(n1 + "%" + n2 + "=" + n1%n2);
+	System.out.println(d1 + "/" + n2 + "=" + d1/n2);
+	System.out.println(n6 + "+" + n7 + "/" + 2 + "=" + n6+n7/2);
+	System.out.println(s1 + s3 + s2);
+	System.out.println(n3 + "+" + n4 + "/" + 5 + '-' + n5 + "%" + n8 + "=" + (n3+n4/5-n5%n8));
+	System.out.println(d2 + "*" + "(" + n6 + "/" + n9 + ")" + "=" + d2*(n6/n9));
+	System.out.println("(" + d2 + "*" +  n6 + ")" + "/" + n9 + "=" + (d2*n6)/n9);
+	}
+}
